73669cebb8677cec7d9f3cc171e8c5acd31e1909
Dmitry Kuzmin <Dmitry.Kuzmin2@evraz.com>
2023-09-28 02:15:31 +0700

    feature: добавить  интернацтонализацию

---
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -0,0 +1,30 @@
+import {FC} from 'react';
+import {useTranslation} from "react-i18next";
+
+import {ClassName} from "shared/ui/types";
+import {joinClassNames as cn} from "shared/lib/utils";
+import {Button} from "shared/ui";
+
+import cls from './LangSwitcher.module.scss';
+
+interface LangSwitcherProps extends ClassName {}
+
+const LangSwitcher: FC<LangSwitcherProps> = (props) => {
+    const { className } = props;
+    const { t,  i18n} = useTranslation();
+
+    const handleLangToggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+    }
+
+    return (
+        <Button
+            className={cn(cls.LangSwitcher, undefined, [className])}
+            onClick={handleLangToggle}
+        >
+            {t('lang')}
+        </Button>
+    );
+};
+
+export default LangSwitcher;
