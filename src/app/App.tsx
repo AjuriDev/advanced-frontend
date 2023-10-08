0c1b4e1b5aa67b1aecc9b50b828df4aa3d34fd4c
AjuriDK <kuzmindmitriy24.03@gmail.com>
2023-10-08 20:18:17 +0700

    feature: Удалить модальное окно из App и создать файл .stylelintignore

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,18 @@
-import { FC, useCallback, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { FC } from 'react';
 
 import { AppRouter } from 'app/providers/Router';
 import { Header } from 'widgets/Header';
 import { Sidebar } from 'widgets/Sidebar';
-import { Button, Modal } from 'shared/ui';
 
 import './styles/_index.scss';
 
-const App: FC = () => {
-  const [opened, setOpened] = useState<boolean>(false);
-  const { t } = useTranslation('common');
-
-  const handleClose = useCallback(() => setOpened(false), []);
-
-  return (
-    <div className="app">
-      <Header />
-      <div className="app__content">
-        <Button onClick={() => setOpened(true)}>{t('actions.open')}</Button>
-        <Sidebar />
-        <AppRouter />
-        <Modal opened={opened} onClose={handleClose} />
-      </div>
+const App: FC = () => (
+  <div className="app">
+    <Header />
+    <div className="app__content">
+      <Sidebar />
+      <AppRouter />
     </div>
-  );
-};
+  </div>
+);
 export default App;
